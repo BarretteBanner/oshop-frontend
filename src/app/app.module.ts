7c14b93af961bef2860b379c2f2369a4416934b5
BarretteBanner <barrettebanner@gmail.com>
2020-03-09 11:40:16 -0500

    Added firebase and bootstrap navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, BsNavbarComponent],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC2mRqFWTFZpKepNB4caMPV4t4Geg4VAgM',
+    authDomain: 'oshop-b45eb.firebaseapp.com',
+    databaseURL: 'https://oshop-b45eb.firebaseio.com',
+    projectId: 'oshop-b45eb',
+    storageBucket: 'oshop-b45eb.appspot.com',
+    messagingSenderId: '415428370749',
+    appId: '1:415428370749:web:45bc454392d80683ece554'
+  }
 };
