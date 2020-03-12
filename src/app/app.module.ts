13dcdadfb29262a3ea1164f57fcace892a5178c2
BarretteBanner <52544117+BarretteBanner@users.noreply.github.com>
2020-03-12 14:43:54 -0500

    [a2dbbf12] deployed to firebase (#14)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
