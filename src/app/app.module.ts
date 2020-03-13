ac37a95acc1b03a879300abff142fe025e2c5ee8
BarretteBanner <52544117+BarretteBanner@users.noreply.github.com>
2020-03-13 14:36:54 -0500

    Implemented login (#18)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AuthService } from './auth.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -53,7 +54,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     NgbModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(public auth: AuthService) {}
+
+  login() {
+    this.auth.login();
+  }
+}
